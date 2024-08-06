8460975e1bbe381d74893be287736db147bb2d63
0jun01 <dudwns85064843@gmail.com>
2024-08-06 11:14:42 +0900

    exceptionHandler 처리완료

--- src/main/java/com/tenco/bank/controller/Test1Controller.java
+++ src/main/java/com/tenco/bank/controller/Test1Controller.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.tenco.bank.handler.exception.unAuthorizedException;
+import com.tenco.bank.handler.exception.UnAuthorizedException;
 import com.tenco.bank.repository.model.User;
 
 // Controller -> String (뷰리졸버 동작 --> JSP 파일 찾아서 렌더링 처리 한다.)
@@ -19,7 +19,7 @@ public class Test1Controller {
 		try {
 			int result = 10 / 0;
 		} catch (Exception e) {
-			throw new unAuthorizedException("인증이 안된 사용자 입니다.", HttpStatus.UNAUTHORIZED);
+			throw new UnAuthorizedException("인증이 안된 사용자 입니다.", HttpStatus.UNAUTHORIZED);
 		}
 		return User.builder().username("길동").password("asd123").build();
 	}
