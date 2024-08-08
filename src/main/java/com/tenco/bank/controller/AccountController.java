3594fecd1ac4c1ffb1e4434a29d333544fd984c8
0jun01 <dudwns85064843@gmail.com>
2024-08-08 10:04:06 +0900

    계좌목록기능구현

--- src/main/java/com/tenco/bank/controller/AccountController.java
+++ src/main/java/com/tenco/bank/controller/AccountController.java
@@ -1,15 +1,20 @@
 package com.tenco.bank.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.tenco.bank.dto.SaveDTO;
 import com.tenco.bank.handler.exception.DataDeliveryException;
 import com.tenco.bank.handler.exception.UnAuthorizedException;
+import com.tenco.bank.repository.model.Account;
 import com.tenco.bank.repository.model.User;
 import com.tenco.bank.service.AccountService;
 
@@ -22,60 +27,82 @@ public class AccountController {
 	// 계좌 생성 화면 요청 DI 처리
 	private final HttpSession session;
 	private AccountService accountService;
-	
+
 	@Autowired
 	public AccountController(HttpSession session, AccountService accountService) {
 		this.session = session;
 		this.accountService = accountService;
 	}
-	
+
 	/**
-	 * 계좌 생성 페이지 요청
-	 * 주소 설계 : http://localhost:8080/account/save
+	 * 계좌 생성 페이지 요청 주소 설계 : http://localhost:8080/account/save
+	 * 
 	 * @return save.jsp
 	 */
 	@GetMapping("/save")
 	public String savePage() {
-		
+
 		// 1. 인증 검사가 필요(account 전체 필요함)
-		User principal = (User)session.getAttribute("principal");
-		if(principal == null) {
+		User principal = (User) session.getAttribute("principal");
+		if (principal == null) {
 			throw new UnAuthorizedException("인증된 사용자가 아닙니다.", HttpStatus.UNAUTHORIZED);
 		}
-		
+
 		return "account/save";
 	}
-	
+
 	// 1. insert때리기
 	// 2. 페이지 홈으로 보내기
 	// 3. 예외처리
-	
+
 	@PostMapping("/save")
 	public String saveProc(SaveDTO dto) {
 		// 1. form 데이터 추출 (파싱 전략)
 		// 2. 인증 검사
 		// 3. 유효성 검사
 		// 4. 서비스 호출
-		User principal = (User)session.getAttribute("principal");
-		if(principal == null) {
+		User principal = (User) session.getAttribute("principal");
+		if (principal == null) {
 			throw new UnAuthorizedException("인증된 사용자가 아닙니다.", HttpStatus.UNAUTHORIZED);
 		}
-		
-		if(dto.getNumber() == null || dto.getNumber().isEmpty()) {
+
+		if (dto.getNumber() == null || dto.getNumber().isEmpty()) {
 			throw new DataDeliveryException("계좌 번호를 입력하세요", HttpStatus.BAD_REQUEST);
-		} else if(dto.getPassword() == null || dto.getPassword().isEmpty()) {
+		} else if (dto.getPassword() == null || dto.getPassword().isEmpty()) {
 			throw new DataDeliveryException("계좌 비밀번호를 입력하세요", HttpStatus.BAD_REQUEST);
-		} else if(dto.getBalance() == null || dto.getBalance() <= 0) {
+		} else if (dto.getBalance() == null || dto.getBalance() <= 0) {
 			throw new DataDeliveryException("계좌 잔액을 입력하세요", HttpStatus.BAD_REQUEST);
 		}
-		
-		
+
 		// 서비스 객체로 전달
-		if(principal.getPassword().equals(dto.getPassword())) {
-			accountService.createAccount(dto, principal.getId());
-		}
-		
-		
+		accountService.createAccount(dto, principal.getId());
+
 		return "redirect:/index";
+	}
+
+	/**
+	 * 계좌 목록 화면 요청 주소 설계는 : http://localhost:8080/account/list, ..../
+	 * 
+	 * @return
+	 */
+	@GetMapping({ "/list", "/" })
+	public String listPage(Model model) {
+
+		// 1. 인증검사
+		User principal = (User) session.getAttribute("principal");
+		if (principal == null) {
+			throw new UnAuthorizedException("인증된 사용자가 아닙니다.", HttpStatus.UNAUTHORIZED);
+		}
+		// 2. 유효성 검사
+		// 3. 서비스 호출
+		List<Account> accountList = accountService.readAccountListByUserId(principal.getId());
+		if (accountList.isEmpty()) {
+			model.addAttribute("accountList", null);
+		} else {
+			model.addAttribute("accountList", accountList);
+		}
+		// JSP 데이터를 넣어 주는 방법
+
+		return "account/list";
 	}
 }
